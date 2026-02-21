ba333d4e122b780414203caf2032ae5da195f317
Aimeric Landou <aimeric.landou@gmail.com>
2026-02-21 06:11:14 +0100

    Undo Fix

--- Utilities/Fits.h
+++ Utilities/Fits.h
@@ -1,8 +1,6 @@
 #ifndef FITS_H
 #define FITS_H
 
-#include "TGraph.h"
-
 TGraphErrors* getFunctionTGraphErrorsFromFitResult(double* xRangeFit, TF1* fitFunctionDrawn, TFitResultPtr fitResult, int nPointsGraph = 1000);
 TGraphErrors* getFunctionTGraphErrorsFromCovMatrix(double* xRangeFit, TF1* fitFunctionDrawn, TMatrixDSym* covMatrix, int nPointsGraph = 1000);
 std::tuple<TF1*, TMatrixDSym, TFitResultPtr> TsallisFit(TH1D* &histogramInput, double* xRangeFit);
